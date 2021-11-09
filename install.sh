ea6954f2f2ce5550b77e99d090cc256e8e2a016d
Michael Bolliger <me@michaelbolliger.com>
2021-11-09 12:28:13 +0100

    cleaned up install

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-mkdir ~/.config/youtube-dl
-mkdir ~/.config/youtube-dl/config
+mkdir -p ~/.config/youtube-dl/config
 ln -sf ~/dotfiles/youtube-dl_config ~/.config/youtube-dl/config
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
