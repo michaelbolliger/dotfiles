b572e9716ea24fe298f184d002ffa65439037623
Michael Bolliger <me@michaelbolliger.com>
2024-04-18 18:45:57 +0200

    updated install script for all config files

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@
 mkdir -p ~/.config/youtube-dl/config
 ln -sf ~/dotfiles/youtube-dl_config ~/.config/youtube-dl/config
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.skhdrc ~/.skhdrc
